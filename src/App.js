25cf66e3a629cf0210d2044534cc5249f83907f2
alaaalh <alaaalh437@gmail.com>
2022-11-15 16:08:24 +0200

    update message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Circle CI
+          Hello Circle CI from Netlify
         </p>
         <a
           className="App-link"
